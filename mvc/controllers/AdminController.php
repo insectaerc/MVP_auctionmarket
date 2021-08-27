ca339cad25e2473b7bcab8c6614f3da412896b8f
TNQA1 <81133854+TNQA1@users.noreply.github.com>
2021-08-27 14:02:55 +0700

    [Add] Admin login function
    Added login functionality.
    
    wip - admin dashboard

---
+++ mvc/controllers/AdminController.php
@@ -0,0 +1,49 @@
+<?php
+class AdminController extends BaseController{
+    public static $instance;
+    private $adminModel;
+    public function __construct()
+    {
+        //Create Admin Model
+        self::$instance = $this;
+        self::loadModel('mvc/models/AdminModel.php');
+        $this->adminModel = new AdminModel;
+    }
+    function index()
+    {
+        if(isset($_SESSION['email'])){
+            $data = $this->adminModel->getAdmins();
+           
+            $model = $this->adminModel;
+            parent::view('mvc/views/frontend/admin/index.php',$data);
+        }else{
+            parent::view('mvc/views/frontend/admin/adminlogin.php');
+        }
+    }
+    
+    function check_admin()
+    {
+        // Kiểm tra nếu người dùng đã ân nút đăng nhập thì mới xử lý
+        if (isset($_POST["adminlogin"])) {
+            // lấy thông tin người dùng
+            $email = $_POST["email"];
+            $password = $_POST["password"];
+            //làm sạch thông tin, xóa bỏ các tag html, ký tự đặc biệt 
+            //mà người dùng cố tình thêm vào để tấn công theo phương thức sql injection
+            $email = strip_tags($email);
+            $email = addslashes($email);
+            $password = strip_tags($password);
+            $password = addslashes($password);
+            if ($email == "" || $password == "") {
+                echo "username or password cannot be blank!";
+            } else {
+                if ($this->adminModel == null) 
+                {
+                    $this->adminModel = new AdminModel;                
+                   
+                }
+                $this->adminModel->check_admin($email, $password);
+            }
+        }
+    }
+}
---
+++ mvc/views/frontend/admin/adminlogin.php
@@ -0,0 +1,40 @@
+
+
+
+
+
+<?php
+    require 'mvc\views\frontend\nav.php';
+    ?>
+
+<?php
+        //echo "This is Button1 that is selected";
+        $controller = AdminController::$instance;
+        if (isset($_POST['adminlogin'])) {
+            $controller::$instance->check_admin();
+        }
+        ?>
+
+<!-- Admin Login form -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Admin Login</title>
+	<link href="../css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+</head>
+<body>
+	<h2 align="center">Admin Login Page </h2>
+	<div class="col-md-6">
+	<div class="form-group">
+		<form action="" method="POST">
+		<label>Email</label>
+		<input type="text" name="email" placeholder="Email" class="form-control"><br>
+		<label>Password</label>
+		<input type="password" name="password" placeholder="password" class="form-control"><br>
+		<input type="submit" name="adminlogin" class="btn btn-primary" value="Login">
+		</form>
+	</div>
+	</div>
+</body>
+</html>
