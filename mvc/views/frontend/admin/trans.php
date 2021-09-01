e5df9b5ed21e8b0ff43e988fb7ac2d8eff9782fe
TNQA1 <81133854+TNQA1@users.noreply.github.com>
2021-09-02 01:31:43 +0700

    Updated trans.php to show list of transaction
    wip - transaction search and search by time

--- mvc/views/frontend/admin/trans.php
+++ mvc/views/frontend/admin/trans.php
@@ -12,9 +12,51 @@
 
 <body>
 
-<?php 
-        include 'mvc/views/frontend/navadmin.php';
-    ?>
-<br> </br>
+    <?php include 'mvc/views/frontend/navadmin.php'; ?>
+    <br> </br>
 
-    <h1 align="center" style="color:Peru"; > Transaction Management </h1>
+    <h1 align="center" style="color:Peru" ;> Transaction Management </h1>
+    <!-- Search transactions -->
+    <div class="form-row">
+        <form action="/MVP_auctionmarket/admin/search/" method="post">
+            <div class="col">
+                <input type="text" class="form-control" placeholder="Search transaction" name="search">
+            </div>
+    </div>
+    </form>
+
+
+
+
+
+    <div class='mx-5 my-3'>
+        <table class="table table-hover text-center">
+            <thead>
+                <tr>
+                    <th scope="col">Transaction ID</th>
+                    <th scope="col">Product ID</th>
+                    <th scope="col">Owner ID</th>
+                    <th scope="col">Bidder ID</th>
+                    <th scope="col">Amount</th>
+                    <th scope="col">Created At</th>
+                </tr>
+            </thead>
+            <tbody>
+                <!-- Show all transactions -->
+                <?php foreach ($data as $entry) { ?>
+                    <tr>
+                        <td> <?php echo $entry['transaction_id']; ?></td>
+                        <td> <?php echo $entry['product_id']; ?></td>
+                        <td> <?php echo $entry['owner_id']; ?></td>
+                        <td> <?php echo $entry['bidder_id']; ?></td>
+                        <td> <?php echo $entry['amount']; ?></td>
+                        <td> <?php echo $entry['createdAt']; ?></td>
+
+                    </tr>
+                <?php } ?>
+
+            </tbody>
+        </table>
+</body>
+
+</html>
