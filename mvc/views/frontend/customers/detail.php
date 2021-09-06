8cdee3c5b71935514259a9d05bd34749e77b8b6c
insectaerc <agilastic@gmail.com>
2021-09-06 15:05:57 +0700

    Update UI of Customer Detail, Profile pages

--- mvc/views/frontend/customers/detail.php
+++ mvc/views/frontend/customers/detail.php
@@ -14,29 +14,49 @@
     ?>
     
     <div class="container">
-        <form action="" method="post" class="register-form">
-            <div class='row'>
-                <div class='col'><h1> Profile</h1></div>
+        <h1> Profile</h1>
+        <hr>
+        <div class='row'>
+            <div class='col'></div>
+            <div class='col' >
+                <h3>Picture</h3>
+                <div class='card' style='height: 250px;'>
+                    <img id='Avatar' class='mb-3' src='/MVP_auctionmarket/upload/<?php echo $data['customer_id']; ?>' alt='Profile Picture' style='height: 250px;'>
+                </div>
             </div>
-            <hr>
+            <div class='col'></div>
+        </div>
+        <hr>
+        <div class='card border-light mb-3'>
+            <div class="card-header h3">Information</div>
+            <div class="card-body">
+                <div class='row'>
+                    <div class='col'>
+                        <label for="email" class="form-label mt-4"><b>Email</b></label>
+                        <input type="email" class="form-control" placeholder="Email" name="email" id="email" disabled value="<?php echo $data['email']; ?>">
 
-            <label for="email" class="form-label mt-4"><b>Email</b></label>
-            <input type="email" class="form-control" placeholder="Email" name="email" id="email" disabled value="<?php echo $data['email']; ?>">
+                        <label for="phone" class="form-label mt-4"><b>Phone</b></label>
+                        <input type="tel" class="form-control" placeholder="Phone Number" name="phone" id="phone" disabled value="<?php echo $data['phone']; ?>">
 
-            <label for="phone" class="form-label mt-4"><b>Phone</b></label>
-            <input type="tel" class="form-control" placeholder="Phone Number" name="phone" id="phone" disabled value="<?php echo $data['phone']; ?>">
+                        <label for="firstName" class="form-label mt-4"><b>First Name</b></label>
+                        <input type="text" class="form-control" placeholder="first name" name="firstName" id="firstName" disabled value="<?php echo $data['first_name']; ?>">
 
-            <label for="firstName" class="form-label mt-4"><b>First Name</b></label>
-            <input type="text" class="form-control" placeholder="first name" name="firstName" id="firstName" disabled value="<?php echo $data['first_name']; ?>">
+                        <label for="lastName" class="form-label mt-4"><b>Last Name</b></label>
+                        <input type="text" class="form-control" placeholder="last name" name="lastName" id="lastName" disabled value="<?php echo $data['last_name']; ?>">
+                    </div>
+                    <div class='col'>
+                        <label for="gender" class="form-label mt-4">Gender:</label>
+                        <input type="text" class="form-control" placeholder="Gender" name="gender" id="gender" disabled value="<?php echo $data['gender']; ?>">
 
-            <label for="lastName" class="form-label mt-4"><b>Last Name</b></label>
-            <input type="text" class="form-control" placeholder="last name" name="lastName" id="lastName" disabled value="<?php echo $data['last_name']; ?>">
+                        <label for="city" class="form-label mt-4"><b>City</b></label>
+                        <input type="text" class="form-control" placeholder="city" name="city" id="city" disabled value="<?php echo $data['city']; ?>">
 
-            <label for="city" class="form-label mt-4"><b>City</b></label>
-            <input type="text" class="form-control" placeholder="city" name="city" id="city" disabled value="<?php echo $data['city']; ?>">
-
-            <hr>
-        </form>
+                        <label for="country" class="form-label mt-4"><b>Country</b></label>
+                        <input type="text" class="form-control" placeholder="country" name="country" id="country" disabled value="<?php echo $data['country']; ?>">
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
 </body>
--- mvc/views/frontend/customers/index.php
+++ mvc/views/frontend/customers/index.php
@@ -14,55 +14,72 @@
     ?>
 
     <div class="container">
-        <form action="" method="post" class="register-form">
-            <div class='row'>
-                <div class='col'>
-                    <h1> Profile</h1>
-                </div>
-                <div class='col'>
-                    <a href="/MVP_auctionmarket/customer/inventory">
-                        <button type="button" class="btn btn-success" style="float:right">Inventory</button>
-                    </a>
+        <div class='row'>
+            <div class='col'>
+                <h1> Profile</h1>
+            </div>
+            <div class='col'>
+                <a href="/MVP_auctionmarket/customer/inventory">
+                    <button type="button" class="btn btn-success" style="float:right">Inventory</button>
+                </a>
+            </div>
+        </div>
+        <hr>
+        <div class='row mb-3'>
+            <div class='col-2'></div>
+            <div class='col-4'>
+                <h3>Picture</h3>
+                <div class='card' style='height: 250px' >
+                    <img id='Avatar' class='mb-3' src='/MVP_auctionmarket/upload/<?php echo $data['customer_id']; ?>' alt='Profile Picture' style='height: 250px'>
                 </div>
             </div>
-            <hr>
-            
-            <img id='Avatar' class='mb-3' src='/MVP_auctionmarket/upload/<?php echo $data['customer_id']; ?>' alt='' style=' max-height: 256px;'>
-            <br>
+            <div class='col-4'>
+                <label for="balance" class="form-label mt-4"><b>Balance</b></label>
+                <input type="balance" class="form-control" placeholder="balance" name="balance" id="balance" disabled value="<?php echo '$'.number_format($data['balance'],2) ?>">
+            </div>
+            <div class='col-2'></div>
+        </div>
+        <hr>
+        <div class='card border-light mb-3'>
+            <div class="card-header h3">Information</div>
+            <div class="card-body">
+                <div class='row'>
+                    <div class='col'>
+                        <label for="email" class="form-label mt-4"><b>Email</b></label>
+                        <input type="email" class="form-control" placeholder="Email" name="email" id="email" disabled value="<?php echo $data['email']; ?>">
 
-            <label for="email" class="form-label mt-4"><b>Email</b></label>
-            <input type="email" class="form-control" placeholder="Email" name="email" id="email" disabled value="<?php echo $data['email']; ?>">
+                        <label for="phone" class="form-label mt-4"><b>Phone</b></label>
+                        <input type="tel" class="form-control" placeholder="Phone Number" name="phone" id="phone" disabled value="<?php echo $data['phone']; ?>">
 
-            <label for="phone" class="form-label mt-4"><b>Phone</b></label>
-            <input type="tel" class="form-control" placeholder="Phone Number" name="phone" id="phone" disabled value="<?php echo $data['phone']; ?>">
+                        <label for="psw" class="form-label mt-4"><b>Password</b></label>
+                        <input type="password" class="form-control" placeholder="Password" name="psw" id="psw" disabled value="<?php echo $data['pass']; ?>">
 
-            <label for="psw" class="form-label mt-4"><b>Password</b></label>
-            <input type="password" class="form-control" placeholder="Password" name="psw" id="psw" disabled value="<?php echo $data['pass']; ?>">
+                        <label for="firstName" class="form-label mt-4"><b>First Name</b></label>
+                        <input type="text" class="form-control" placeholder="first name" name="firstName" id="firstName" disabled value="<?php echo $data['first_name']; ?>">
 
-            <label for="firstName" class="form-label mt-4"><b>First Name</b></label>
-            <input type="text" class="form-control" placeholder="first name" name="firstName" id="firstName" disabled value="<?php echo $data['first_name']; ?>">
+                        <label for="lastName" class="form-label mt-4"><b>Last Name</b></label>
+                        <input type="text" class="form-control" placeholder="last name" name="lastName" id="lastName" disabled value="<?php echo $data['last_name']; ?>">
+                    </div>
+                    <div class='col'>
+                        <label for="city" class="form-label mt-4"><b>City</b></label>
+                        <input type="text" class="form-control" placeholder="city" name="city" id="city" disabled value="<?php echo $data['city']; ?>">
 
-            <label for="lastName" class="form-label mt-4"><b>Last Name</b></label>
-            <input type="text" class="form-control" placeholder="last name" name="lastName" id="lastName" disabled value="<?php echo $data['last_name']; ?>">
+                        <label for="country" class="form-label mt-4"><b>Country</b></label>
+                        <input type="text" class="form-control" placeholder="country" name="country" id="country" disabled value="<?php echo $data['country']; ?>">
 
-            <label for="city" class="form-label mt-4"><b>City</b></label>
-            <input type="text" class="form-control" placeholder="city" name="city" id="city" disabled value="<?php echo $data['city']; ?>">
+                        <label for="nationalId" class="form-label mt-4"><b>National ID</b></label>
+                        <input type="text" class="form-control" placeholder="National Id" name="nationalId" id="nationalId" disabled value="<?php echo $data['national_id']; ?>">
 
-            <label for="country" class="form-label mt-4"><b>Country</b></label>
-            <input type="text" class="form-control" placeholder="country" name="country" id="country" disabled value="<?php echo $data['country']; ?>">
+                        <label for="branch" class="form-label mt-4">Branch:</label>
+                        <input type="text" class="form-control" placeholder="Branch" name="branch" id="branch" disabled value="<?php echo $data['branch_id']; ?>">
 
-            <label for="nationalId" class="form-label mt-4"><b>National Id</b></label>
-            <input type="text" class="form-control" placeholder="National Id" name="nationalId" id="nationalId" disabled value="<?php echo $data['national_id']; ?>">
-
-            <label for="branch" class="form-label mt-4">Branch:</label>
-            <input type="text" class="form-control" placeholder="Branch" name="branch" id="branch" disabled value="<?php echo $data['branch_id']; ?>">
-
-            <label for="gender" class="form-label mt-4">Gender:</label>
-            <input type="text" class="form-control" placeholder="Gender" name="gender" id="gender" disabled value="<?php echo $data['gender']; ?>">
-            <hr>
-        </form>
+                        <label for="gender" class="form-label mt-4">Gender:</label>
+                        <input type="text" class="form-control" placeholder="Gender" name="gender" id="gender" disabled value="<?php echo $data['gender']; ?>">
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-
 </body>
 
 </html>
