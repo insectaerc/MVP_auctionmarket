989b174b771676906d4f1d5efb2873926b32de6e
insectaerc <agilastic@gmail.com>
2021-09-12 14:14:43 +0700

    Update sql-Init.sql

--- SQL/sql-Init.sql
+++ SQL/sql-Init.sql
@@ -9,6 +9,8 @@ USE
 
 DROP TABLE IF EXISTS branches;
 DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS transactions;
+DROP TABLE IF EXISTS admin;
 
 CREATE TABLE branches
 (
@@ -54,6 +56,16 @@ CREATE TABLE transactions
 	FOREIGN KEY (bidder_id) REFERENCES customers (customer_id)
 );
 
+CREATE TABLE admin
+(
+    admin_id      INT      NOT NULL,
+    email    VARCHAR(100)  NOT NULL,
+    password   VARCHAR(30)  NOT NULL,
+    admin_name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (admin_id),
+    UNIQUE (admin_name)
+);
+
 INSERT INTO `branches` (`branch_id`,`branch_name`,`branch_phone`,`branch_address`) VALUES 
 ("B1","West Branch","(02) 3699 2373","P.O. Box 992, 7935 Ornare Road"),
 ("B2","East Branch","(09) 5345 3753","7715 Sociis St."),
@@ -74,16 +86,10 @@ VALUES
 	(9,"1998-10-06","Hayes","Hutchinson","F","commodo.auctor.velit@metusInnec.ca","Hoogeveen","Palau","D0955C6B-F8A4-E3F8-6CCE-59300A0D158A","(05) 8802 5171","1234",57292),
 	(10,"1982-09-08","Nola","Witt","F","magna.tellus@aduiCras.co.uk","Branchon","Belgium","EA07C0F7-5FAA-90E0-7CEB-981057B87635","(01) 4664 3322","1234",49559);
 
-USE AUCTIONMARKET;
-
-CREATE TABLE admin
-(
-    admin_id      INT      NOT NULL,
-    email    VARCHAR(100)  NOT NULL,
-    password   VARCHAR(30)  NOT NULL,
-    admin_name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (admin_id),
-    UNIQUE (admin_name)
-);
-
 INSERT INTO `admin` (`admin_id`, `email`, `password`, `admin_name`) VALUES ('1', 'admin@gmail.com', '1234', 'John Admin');
+
+ALTER TABLE customers
+ADD INDEX customer_phone_idx (phone)
+
+ALTER TABLE transactions
+ADD INDEX trans_productId_indx (product_id)
